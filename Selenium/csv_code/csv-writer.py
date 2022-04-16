6e98b95cb6fa787c3801f844a72de0f167089993
Snir10 <61028802+Snir10@users.noreply.github.com>
2022-04-17 00:57:27 +0300

    working csv-writer from web 2

--- Selenium/csv_code/csv-writer.py
+++ Selenium/csv_code/csv-writer.py
@@ -1,5 +1,16 @@
 import csv
 
+
+def open_and_set_data(head, data):
+    with open('asins.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(head)
+
+        # write multiple rows
+        writer.writerows(data)
+
 header = ['name', 'area', 'country_code2', 'country_code3']
 data = [
     ['Albania', 28748, 'AL', 'ALB'],
@@ -7,13 +18,5 @@ data = [
     ['American Samoa', 199, 'AS', 'ASM'],
     ['Andorra', 468, 'AD', 'AND'],
     ['Angola', 1246700, 'AO', 'AGO']
-]
-
-with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-
-    # write the header
-    writer.writerow(header)
-
-    # write multiple rows
-    writer.writerows(data)
+        ]
+open_and_set_data(header, data)
