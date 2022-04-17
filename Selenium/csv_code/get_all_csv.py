45b51e62d7578ed1b4c37abfb77b23e278cecbf5
Snir10 <61028802+Snir10@users.noreply.github.com>
2022-04-17 21:16:34 +0300

    bug fixes

--- Selenium/csv_code/get_all_csv.py
+++ Selenium/csv_code/get_all_csv.py
@@ -1,28 +1,39 @@
 import os
 import csv
 
-all_files = os.listdir('/Users/user/Downloads/Helium_10_ASIN_Grabber')
-csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
+
+def get_csv_files(dir):
+    csv_counter = 0
+    all_files = os.listdir(dir)
+    csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
+
+    # lambda returns True if filename (within `all_files`) ends with .csv or else False
+    # and filter function uses the returned boolean value to filter .csv files from list files.
+
+    csv_counter = len(csv_files)
+    print(csv_files)
+    return csv_files
+
+
+def print_csv_content(csv_files):
+    counter = 0
+    for csv_file in csv_files:
+        file = open('/Users/user/Downloads/Helium_10_ASIN_Grabber/' + csv_file)
+        print(csv_file)
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        # print(header)
+        rows = []
+        for row in csvreader:
+            counter += 1
+            rows.append(row)
+            print('ASIN: ' + row[1] + '\t' + 'NUM: ' + str(counter) + '\t' + row[3] + '\t' + row[4] + '\t' + row[5])
+        # print(rows)
+
+        file.close()
+
+
 asin_counter = 0
-csv_counter = len(csv_files)
-print(csv_files)
-
-# lambda returns True if filename (within `all_files`) ends with .csv or else False
-# and filter function uses the returned boolean value to filter .csv files from list files.
-
-for csv_file in csv_files:
-    file = open('/Users/user/Downloads/Helium_10_ASIN_Grabber/'+csv_file)
-    print(csv_file)
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    #print(header)
-    rows = []
-    for row in csvreader:
-        rows.append(row)
-        asin_counter += 1
-        print(row[1]+'\tCounter:'+str(asin_counter))
-    # print(rows)
-
-    file.close()
-
-print('CSV Files Count is: '+str(csv_counter))
+src_dir = '/Users/user/Downloads/Helium_10_ASIN_Grabber'
+csv_files = get_csv_files(src_dir)
+print_csv_content(csv_files)
