42ba952e658fb53f9283af0dad298232b8356bfd
Snir10 <61028802+Snir10@users.noreply.github.com>
2023-06-13 00:40:14 +0300

    fixed fex things all running good

--- yupoo/yupoo_iterator.py
+++ yupoo/yupoo_iterator.py
@@ -1,21 +1,17 @@
 import os
 import csv
 import re
-
 import requests
 from urllib.parse import urljoin
-import logging
-
+from time import sleep
+from aliexpress_api import AliexpressApi, models
 from colorlog import ColoredFormatter
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
+import logging
 def logger_init():
     log = logging.getLogger('my_module_name')
     log.setLevel(level=logging.DEBUG)
@@ -28,107 +24,246 @@ def logger_init():
     log.addHandler(fh)
 
     return log
-
-def download_images(album_urls, destination_folder):
-    csv_file = os.path.join(destination_folder, 'album_data.csv')
-    with open(csv_file, 'a', newline='') as f:
-        writer = csv.writer(f)
-        if f.tell() == 0:  # Check if the file is empty
-            writer.writerow(['ID', 'Title', 'Subtitle Content', 'Link', 'Images'])
-        else:
-            f.seek(0, os.SEEK_END)  # Move the file pointer to the end
-
-        for album_index, album_url in enumerate(album_urls, 1):
-            log.info(f'Album index: {album_index}')
-            album_folder = os.path.join(destination_folder, f'album_{album_index}')
-            os.makedirs(album_folder, exist_ok=True)
-            driver = webdriver.Chrome(service=service, options=options)
-            try:
-                log.debug(f'running get_album_data for album_index: {album_index}')
-                get_album_data(driver, album_url, album_folder, writer)
-            except StaleElementReferenceException:
-                log.error(f"Skipping album {album_index} due to stale element reference")
-
-            driver.quit()
-            f.flush()  # Flush the writer to update the file
-
-def get_album_data(driver, album_url, album_folder, writer):
-    driver.get(album_url)
-
-    title_div = driver.find_element(By.CSS_SELECTOR, 'span.showalbumheader__gallerytitle')
-    subtitle_div = driver.find_element(By.CSS_SELECTOR, 'div.showalbumheader__gallerysubtitle.htmlwrap__main')
-    image_divs = driver.find_elements(By.CSS_SELECTOR, 'div.showalbum__children.image__main')
-
-    title = title_div.text
-    subtitle_content = subtitle_div.text
-
-    match = re.search(r'(https?://\S+)', subtitle_content)
-    link = match.group(1) if match else ''
-
-    id = extract_unique_id(title)
-
-    log.info(f"[ID: {id}] Album Title: {title}\tSubtitle Content: {subtitle_content}\tLink: {link}")
-
-    images = []
-
-    for i, image_div in enumerate(image_divs, 1):
-        img_tag = image_div.find_element(By.CSS_SELECTOR, 'img')
-        image_url = img_tag.get_attribute('src')
-        parsed_url = urljoin(album_url, image_url)
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-            'Referer': album_url
-        }
-
-        image_name = f"image_{i}.png"
-
-        response = requests.get(parsed_url, headers=headers)
-        response.raise_for_status()
-
-        with open(os.path.join(album_folder, image_name), 'wb') as f:
-            f.write(response.content)
-
-        images.append(image_name)
-
-        log.debug(f"Downloaded image: {image_name}")
-    log.info(f"all images: {images}")
-
-    # Prepare album data
-    album_data = [id, title, subtitle_content, link, ','.join(images)]
-    writer.writerow(album_data)
-
-def extract_unique_id(string):
-    pattern = r'AN\d+'  # Pattern for "AN" followed by one or more digits
-
+def createAliExpressInstance():
+    return AliexpressApi('34061046', '3766ae9cf22b81c88134fb56f71eb03c', models.Language.EN, models.Currency.EUR,
+                         'sn2019')
+def get_id(string):
+    #TODO - fix return with log 2 options
     match = re.search(pattern, string)
     if match:
         unique_id = match.group()
+        log.debug(f'ID: {unique_id}')
         return unique_id
     else:
         return 'no ID'
+def setAffiliateLink(next_url, no_link_recived_cnt, link_received_cnt):
+    if next_url.startswith(('https://www.aliexpress.com/item/', 'https://www.aliexpress.us/item',
+                            'https://www.he.aliexpress.com/item')):
+        log.debug(f'sending link to AE {next_url}')
 
-# Example usage:
-main_url = "https://taozi140.x.yupoo.com/albums"
-destination_folder = "/Users/user/Desktop/yupoo"
+        resp = aliexpress.get_affiliate_links(next_url)
+
+        log.debug(f'Received AliExpress Response:\t{resp}')
+
+        if resp and hasattr(resp[0], 'promotion_link') and resp[0].promotion_link.startswith('https'):
+            affiliate_link = resp[0].promotion_link
+            link_received_cnt += 1
+        else:
+            affiliate_link = 'Failed to convert Ali Express link'
+            log.debug(f'No Link from AE{resp}')
+            no_link_recived_cnt += 1
+    else:
+        affiliate_link = 'No Ali Express link detected'
+        log.warning(f'Bad link -> {next_url}')
+        no_link_recived_cnt += 1
+    return affiliate_link, no_link_recived_cnt, link_received_cnt
+def download_album_images(album_folder, album_url, image_divs):
+    images = []
+
+    image_divs = image_divs[:1]
+
+    try:
+        log.debug(f"Going to download {len(image_divs)} images")
+        for i, image_div in enumerate(image_divs, 1):
+            img_tag = image_div.find_element(By.CSS_SELECTOR, 'img')
+            image_url = img_tag.get_attribute('src')
+            parsed_url = urljoin(album_url, image_url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+                'Referer': album_url}
+            image_name = f"image_{i}.png"
+            images.append(image_name)
+            response = requests.get(parsed_url, headers=headers)
+            response.raise_for_status()
+            with open(os.path.join(album_folder, image_name), 'wb') as f:
+                f.write(response.content)
+            log.debug(f"Downloaded image: {album_folder}/{image_name}")
+        log.debug(f"all images: {images}")
+    except:
+        log.warning(f"Something went wrong {id} {link} {title} ")
+
+
+    return images
+def get_link_match(subtitle_content):
+    link_match = re.search(r'(https?://\S+)', subtitle_content)
+    # log.debug(f'Link match: {link_match}')
+
+    return link_match
+def get_price(title):
+    try:
+        price = re.search(r'\d+(\.\d+)?\$', title).group()
+    except:
+        price = 'no price'
+    log.debug(f'price: {price}')
+
+    return price
+def get_image_divs(driver):
+    image_divs = driver.find_elements(By.CSS_SELECTOR, 'div.showalbum__children.image__main')
+    return image_divs
+def get_subtitle_content(driver):
+    subtitle_content = driver.find_element(By.CSS_SELECTOR, 'div.showalbumheader__gallerysubtitle.htmlwrap__main').text
+    return subtitle_content
+def get_title(driver):
+    title = driver.find_element(By.CSS_SELECTOR, 'span.showalbumheader__gallerytitle').text
+    log.debug(f'Title: {title}')
+    return title
+def create_album_fd(album_index, dest_fd):
+    album_folder = os.path.join(dest_fd, f'album_{album_index}')
+    os.makedirs(album_folder, exist_ok=True)
+    return album_folder
+def get_album_urls():
+    log.info(f'Processing URL: {url}')
+
+    log.debug(f"Opening main albums url: {main_urls}")
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    album_links = driver.find_elements(By.CSS_SELECTOR, 'div.showindex__children a')
+    log.info(f"Albums Count: Found {len(album_links)} Albums !")
+    album_urls = [album_link.get_attribute('href') for album_link in album_links]
+    log.debug(f"Quitting main albums url: {url}")
+    driver.quit()
+    return album_urls
+def print_to_log(id, images, link, price, resp, title, no_link, link_received):
+    log.info(
+        f"[ID:{id}] [{link_received}/{no_link + link_received}] [IMGs:{len(images)}]  {title} Price: {price} \tlink: {link}\tAff-link: {resp} \tno link counter: {no_link} ")
+def link_validation(link_match):
+    if link_match:  # if no link at all, or no s.click will continue next item
+        log.debug(f'Found link: {link_match}')
+        return link_match.group(1)
+    else:
+        log.warning(f"Moving to next item since val link_match={link_match}")
+        return False
+def resp_validation(r):
+    if not r.startswith('https://s.click.aliexpr'):
+        log.warning(r)
+        return False
+    else:
+        return True
+def print_counters():
+    log.info("Processed all albums.")
+    log.info(f"Counter for albums with no link at all: {counter_no_link_at_all}")
+    log.info(f"Counter for albums with no link received from AE: {counter_no_link_recivied_from_AE}")
+    log.info(f"Counter for valid products: {valid_products_counter}")
+def get_pages_count():
+    # previous use -
+    # main_urls = [f'https://taozi140.x.yupoo.com/albums?page={i}' for i in range(1, 10)]
+    # main_urls = ['https://tianlong980120.x.yupoo.com/albums']
+
+    d = webdriver.Chrome(service=service, options=options)
+    d.get(main_url_templete)
+    element = d.find_element(By.CSS_SELECTOR, 'form.pagination__jumpwrap span')
+    text = element.text
+    match = re.search(r'\d+', text)
+    if match:
+        number = int(match.group())
+        log.warning(number)
+        d.quit()
+        return number
+    else:
+        log.warning(f'no pages found - {main_url_templete}')
+        d.quit()
+
+        return None
+# CSV
+def create_csv():
+    headers = ['ID', 'title', 'price', 'next url', 'aff link', 'album path', 'album items']
+
+    with open(csv_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+def append_to_csv():
+
+    row_data = [id, title, price, link, resp, album_folder, images]
+    with open(csv_path, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(row_data)
+    log.debug('csv updated')
+
+
+
+
 log = logger_init()
 
+
+
+site_id = '767867590'
+pattern = r'TL\d{4}'  # Pattern for "AN" followed by four digits
+
+
+# pattern = r'AN\d{4}'  # Pattern for "AN" followed by four digits
+
+aliexpress = createAliExpressInstance()
 options = Options()
 options.add_argument('--headless')
-
 chromedriver_path = '/path/to/chromedriver'
 service = Service(chromedriver_path)
+noLinkCounter, link_received_cnt = 0, 0
 
-log.info(f"Opening main albums url: {main_url}")
+counter_no_link_at_all = 0
+counter_no_link_recivied_from_AE = 0
+valid_products_counter = 0
 
-driver = webdriver.Chrome(service=service, options=options)
-driver.get(main_url)
+main_url_templete = f'https://{site_id}.x.yupoo.com/albums?page='
 
-album_links = driver.find_elements(By.CSS_SELECTOR, 'div.showindex__children a')
-album_urls = [album_link.get_attribute('href') for album_link in album_links]
-log.info(f"Quiting main albums url: {main_url}")
+# def extract_hidden_number(string):
 
-driver.quit()
-log.info(f'Albums URLs Count: {len(album_urls)}')
 
-download_images(album_urls, destination_folder)
+
+# def get_main_urls():
+
+
+
+# main_urls = get_main_urls()
+pages_count = get_pages_count()
+main_urls = [f'https://{site_id}.x.yupoo.com/albums?page={i}' for i in range(1, pages_count)]
+
+
+
+for index, url in enumerate(main_urls):
+    album_urls = get_album_urls()
+    destination_folder = f'/Users/user/Desktop/YUPOO/yupoo_{index}'
+    csv_path = f'/Users/user/Desktop/YUPOO/Items_{index}.csv'
+    create_csv()
+
+    for album_index, album_url in enumerate(album_urls, 1):
+        if album_index % 10 == 0:
+            log.info(f"Processed {album_index} albums.")
+            print_counters()
+
+        driver = webdriver.Chrome(service=service, options=options)
+        try:
+            driver.get(album_url)
+            title = get_title(driver)
+            subtitle_content = get_subtitle_content(driver)
+            image_divs = get_image_divs(driver)
+            id = get_id(title)
+            price = get_price(title)
+            link_match = get_link_match(subtitle_content)
+            link = link_validation(link_match)
+
+            album_folder = create_album_fd(album_index, destination_folder)
+
+
+            if not link:
+                counter_no_link_at_all += 1
+                resp = 'no link 2323'
+
+                append_to_csv()
+                continue
+
+            resp, no_link_count, link_received_cnt = setAffiliateLink(link, noLinkCounter, link_received_cnt)
+            if not resp_validation(resp):
+                counter_no_link_recivied_from_AE += 1
+                append_to_csv()
+                continue
+
+            valid_products_counter += 1
+
+            images = download_album_images(album_folder, album_url, image_divs)
+            print_to_log(id, images, link, price, resp, title, no_link_count, link_received_cnt)
+            append_to_csv()
+        except StaleElementReferenceException:
+            log.error(f"Skipping album {album_index} due to stale element reference")
+        driver.quit()
+
+    if (index + 1) * 50 >= len(main_urls): print_counters()
